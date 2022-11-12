7bd8928fd2530c3fda32d396dca9f4f6ce6275b5
Sixtus6 <xystuseton@gmail.com>
2022-11-12 18:06:28 +0100

    nodemailer

---
+++ index.js
@@ -0,0 +1,64 @@
+var nodemailer = require('nodemailer');
+
+// var transporter = nodemailer.createTransport({
+ 
+ 
+//   host: "mail.ctrixx.com",
+//   port: 25,
+//   secure: false,
+//   logger: true,
+//   debug: true,
+//   ignoreTLS: true,
+//   auth: {
+//     user: 'hello@ctrixx.com',
+//     pass: 'ctrixx@hello'
+//   }
+// });
+
+// var mailOptions = {
+//   from: 'hello@ctrixx.com',
+//   to: 'alexmencer94@gmail.com',
+//   subject: 'Sending ',
+//   text: 'That was easy!',
+//   html: "<b>Hello world ✔</b>"
+// };
+
+// transporter.sendMail(mailOptions, function(error, info){
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log('Email sent: ' + info.response);
+//   }
+// }); 
+
+var transporter = nodemailer.createTransport({
+ 
+ //input smtp host name
+  host: "",
+//port
+  port: 25,
+  secure: false,
+  logger: true,
+  debug: true,
+  ignoreTLS: true,
+  auth: {
+    user: '',
+    pass: ''
+  }
+});
+
+var mailOptions = {
+  from: 'hello@ctrixx.com',
+  to: 'alexmencer94@gmail.com',
+  subject: 'Sending ',
+  text: 'That was easy!',
+  html: "<b>Hello world ✔</b>"
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+}); 
